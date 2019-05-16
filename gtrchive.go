e6cdda3653c9dc71a24113882e24543ab418f449
John McKenzie <jmckind@gmail.com>
2019-05-15 21:59:49 -0500

    Add initial logic to persist tweet data in RethinkDB

--- gtrchive.go
+++ gtrchive.go
@@ -15,11 +15,13 @@
 package main
 
 import (
+	"fmt"
 	"net/url"
 	"os"
 	"runtime"
 
 	"github.com/ChimeraCoder/anaconda"
+	r "gopkg.in/rethinkdb/rethinkdb-go.v5"
 )
 
 var log = anaconda.BasicLogger
@@ -36,19 +38,47 @@ func main() {
 	)
 	api.Log = log
 
+	if ok, err := api.VerifyCredentials(); !ok || err != nil {
+		log.Fatalf("Invalid credentials. %v", err)
+	}
+
 	params := url.Values{
 		"track": {os.Getenv("GTR_TWITTER_TRACK")},
 	}
 
 	rdbHost := os.Getenv("GTR_RETHINKDB_HOST")
 	rdbPort := os.Getenv("GTR_RETHINKDB_PORT")
+	rdbDatabase := os.Getenv("GTR_RETHINKDB_DATABASE")
 	rdbUsername := os.Getenv("GTR_RETHINKDB_USERNAME")
 	rdbPassword := os.Getenv("GTR_RETHINKDB_PASSWORD")
 
 	log.Debugf("RethinkDB Host: %s", rdbHost)
 	log.Debugf("RethinkDB Port: %s", rdbPort)
+	log.Debugf("RethinkDB Database: %s", rdbDatabase)
 	log.Debugf("RethinkDB Username: %s", rdbUsername)
 	log.Debugf("RethinkDB Password: %s", rdbPassword)
+
+	rdbOpts := r.ConnectOpts{
+		Address:  fmt.Sprintf("%s:%s", rdbHost, rdbPort),
+		Database: rdbDatabase,
+		Username: rdbUsername,
+		Password: rdbPassword,
+	}
+
+	session, err := r.Connect(rdbOpts)
+	if err != nil {
+		log.Fatalf("Unable to connect to database. %v", err)
+	}
+
+	err = r.DBCreate(rdbDatabase).Exec(session)
+	if err != nil {
+		log.Errorf("Unable to create database. %v", err)
+	}
+
+	err = r.TableCreate("tweets").Exec(session)
+	if err != nil {
+		log.Errorf("Unable to create table. %v", err)
+	}
 
 	log.Debugf("Streaming tweets using params: %v", params)
 	stream := api.PublicStreamFilter(params)
@@ -57,6 +87,10 @@ func main() {
 		switch o := obj.(type) {
 		case anaconda.Tweet:
 			log.Debugf("%-15s: %s", o.User.ScreenName, o.Text)
+			err := r.Table("tweets").Insert(o).Exec(session)
+			if err != nil {
+				log.Errorf("Unable to insert database record. %v", err)
+			}
 		}
 	}
 }
